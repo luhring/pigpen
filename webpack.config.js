4444b76d75e88b9765d0ba91c066a46b7d9e1308
Dan Luhring <dan+github@luhrings.com>
2017-08-12 14:27:41 -0400

    Get React working

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+const { CheckerPlugin } = require('awesome-typescript-loader')
+
+module.exports = {
+  entry: "./dist/react-app/index",
+
+ // Currently we need to add '.ts' to the resolve.extensions array.
+ resolve: {
+   extensions: ['.ts', '.tsx', '.js', '.jsx']
+ },
+
+ // Source maps support ('inline-source-map' also works)
+ devtool: 'source-map',
+
+ // Add the loader for .ts files.
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+
+  output: {
+    filename: "./dist/react-app/bundle.js"
+  },
+
+  plugins: [
+      new CheckerPlugin()
+  ]
+};
